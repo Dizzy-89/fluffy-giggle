544a728b45862bec47cf9e722cbd5123297e3823
DCPerez22 <72471462+DCPerez22@users.noreply.github.com>
2021-11-28 05:12:08 -0500

    11/28 Update
    Changes made to Data Exploration, KNN, and Neural Network.
    
    - Catalan Perez, David
    
    Co-Authored-By: Dizzy-89 <64996197+Dizzy-89@users.noreply.github.com>

--- Project_Ideas.R
+++ Project_Ideas.R
@@ -1,21 +1,30 @@
-# Clearing Global Environment
+# Clearing the Global Environment
 rm(list = ls())
 
 # ===== Setting the Environment =====
 set.seed(1)
 options(scipen = 999, digits = 1)
+# Checking for required Libraries
 library(forecast)
 library(caret)
 library(e1071)
 
-# ===== Configuring the Data =====
 # Loading Data set
 loan_df <- read.csv("loan_eligibility_copy.csv", fileEncoding="UTF-8-BOM")
 
+# ===== Exploring the variables =====
+summary(loan_df)
+
+unique(loan_df$Term)
+unique(loan_df$Years.in.current.job)
+unique(loan_df$Home.Ownership)
+unique(loan_df$Purpose)
+
+# ===== Configuring the Data =====
 # Sub-setting: Removing unneeded Categories
 loan_df <- subset(loan_df, select=-c(Loan.ID, Customer.ID, Purpose, Home.Ownership))
 
-# Converting Categorical Variables into Conditional Statements:
+# Converting Categorical Variables into Dummy Variables:
 # 1 = True; 0 = False
 loan_df$Loan.Status <- ifelse(loan_df$Loan.Status == "Fully Paid", 1, 0)
 
@@ -24,14 +33,28 @@ loan_df$Term <- ifelse(loan_df$Term == "Short Term", 1, 0)
 
 # Keeps only the number of years individuals have worked in their current job.
 loan_df$Years.in.current.job <- as.numeric(gsub("([0-9]+).*$", "\\1", loan_df$Years.in.current.job))
+# 1 = 5 or more years; 0 = Less than 5 years
 loan_df$Years.in.current.job <- ifelse(loan_df$Years.in.current.job >= 5, 1, 0)
 # Changing <1 year Experience to 0:
 loan_df$Years.in.current.job[is.na(loan_df$Years.in.current.job)] <- 0
 
 # Changing Months since Last Delinquent
+# 1 = There was a Delinquent; 0 = There was not a Delinquent
 loan_df$Months.since.last.delinquent <- ifelse(loan_df$Months.since.last.delinquent >= 1, 1, 0)
-# Change NA values to 0
+# Change NA (Missing) values to 0
 loan_df$Months.since.last.delinquent[is.na(loan_df$Months.since.last.delinquent)] <- 0
+
+# Changing Bankruptcies
+# 1 = There was a reported Bankruptcy; 0 = There was not a reported Bankruptcy
+loan_df$Bankruptcies <- ifelse(loan_df$Bankruptcies >= 1, 1, 0)
+# Change NA (Missing) values to 0
+loan_df$Bankruptcies[is.na(loan_df$Bankruptcies)] <- 0
+
+# Changing Tax Liens
+# 1 = There was a Tax Lien; 0 = There was not a Tax Lien
+loan_df$Tax.Liens <- ifelse(loan_df$Tax.Liens >= 1, 1, 0)
+# Change NA (Missing) values to 0
+loan_df$Tax.Liens[is.na(loan_df$Tax.Liens)] <- 0
 
 # Editing NA values from Credit Score and Annual Income
 loan_df <- loan_df[!is.na(loan_df$Credit.Score), ]
@@ -41,6 +64,9 @@ loan_df <- loan_df[!loan_df$Credit.Score > 850, ]
 
 # Removing loans with Current Amount of $99999999
 loan_df <- loan_df[!loan_df$Current.Loan.Amount == 99999999, ]
+
+# Checking to see if all Missing Values has been removed
+summary(loan_df)
 
 # ===== Principal Component Analysis (PCA) =====
 cov(na.omit(loan_df))
@@ -65,7 +91,8 @@ pov.cor <- pca.cor$sdev^2 / sum(pca.cor$sdev^2)
 barplot(pov.cor, xlab = "Principal Components", ylab = "Proportion of Variance Explained")
 
 # Adjusting to PCA Normalization
-loan_df_adj <- subset(loan_df, select=-c(Maximum.Open.Credit, Bankruptcies, Tax.Liens))
+loan_df_adj <- subset(loan_df, select=-c(Number.of.Credit.Problems))
+loan_df_adj <- loan_df
 
 # ===== Partitioning the Data =====
 train.index <- sample(c(1:dim(loan_df_adj)[1]), dim(loan_df_adj)[1] * 0.6)
@@ -84,17 +111,17 @@ summ(lm.fit)
 # Evaluate the Model - Training Set
 lm.pred.pro.train <- predict(lm.fit, train.df, type='response')
 lm.pred.train <- factor(ifelse(lm.pred.pro.train > 0.5, 1, 0))
-confusionMatrix(lm.pred.train, factor(train.df$Term), positive='1')
+confusionMatrix(lm.pred.train, factor(train.df$Loan.Status), positive='1')
 
 # Evaluating the Model - Validation Set
 lm.pred.pro.valid <- predict(lm.fit, valid.df, type='response')
 lm.pred.valid <- factor(ifelse(lm.pred.pro.valid > 0.5, 1, 0))
-confusionMatrix(lm.pred.valid, factor(valid.df$Term), positive='1')
+confusionMatrix(lm.pred.valid, factor(valid.df$Loan.Status), positive='1')
 
 # ===== Neural Network (STILL Work-In-Progress) =====
 library(neuralnet)
 # Fit Model
-nn <- neuralnet(Loan.Status ~ ., data = train.df, hidden = 3)
+nn <- neuralnet(Term ~ ., data = train.df, hidden = 3)
 
 # report model
 library(NeuralNetTools)
@@ -104,12 +131,12 @@ plot(nn)
 # evaluate model (training set)
 nn.pred.pro <- compute(nn, train.df)
 nn.pred <- factor(ifelse(nn.pred.pro$net.result > 0.5, 1, 0))
-confusionMatrix(nn.pred, factor(train.df$Loan.Status))
+confusionMatrix(as.factor(nn.pred), as.factor(train.df$Loan.Status), positive = '1')
 
 # evaluate model (validation set)
 nn.pred.pro.valid <- compute(nn, valid.df)
 nn.pred.valid <- factor(ifelse(nn.pred.pro.valid$net.result > 0.5, 1, 0))
-confusionMatrix(nn.pred.valid, factor(valid.df$status))
+confusionMatrix(nn.pred.valid, factor(valid.df$Loan.Status), positive = '1')
 
 
 # ===== Classification Tree =====
@@ -138,17 +165,19 @@ confusionMatrix(as.factor(pred.loan), as.factor(loan_df_adj$Term))
 # ===== K-Nearest Neighbor (STILL Work-In-Progress) =====
 library(class)
 # run kNN with k=5
-nn5 <- knn(train.df, valid.df, cl=as.factor(train.df$Term), k=5)
-confusionMatrix(as.factor(nn5), as.factor(valid.df$Term))
+nn5 <- knn(train.df, valid.df, cl=as.factor(train.df$Loan.Status), k=5)
+confusionMatrix(as.factor(nn5), as.factor(valid.df$Loan.Status))
 
 # Find optimal K (from 1 to 15) in terms of accuracy
 accuracy.df <- data.frame(k=seq(1, 15, 1), accuracy=0)
 for(i in 1:15){
-  knn.pred <- knn(train.df, valid.df, cl=as.factor(train.df$Term), k=i)
+  knn.pred <- knn(train.df, valid.df, cl=as.factor(train.df$Loan.Status), k=i)
   accuracy.df[i, 'accuracy'] <- confusionMatrix(knn.pred, 
-                                                as.factor(valid.df$Term))$overall[1]
+                                                as.factor(valid.df$Loan.Status))$overall[1]
 }
+
 View(accuracy.df)
+
 # ===== Random Forest =====
 #install.packages("randomForest")
 library(randomForest)
